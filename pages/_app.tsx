84bae94080274cc2c0fb63685d6405008068018c
Joshua Immordino <joshimmor@Gmail.com>
2022-05-11 02:07:57 -0400

    final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from "framer-motion"
-Context}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <AnimatePresence>
